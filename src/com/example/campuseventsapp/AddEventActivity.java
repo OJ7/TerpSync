752cb3dbab2468bf7ae2fb431e5139a8065c1c9f
OJ7 <omairj7@gmail.com>
2014-11-22 20:03:10 -0500

    Added (but commented out) SharedPrefs

--- src/com/example/campuseventsapp/AddEventActivity.java
+++ src/com/example/campuseventsapp/AddEventActivity.java
@@ -10,38 +10,74 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
 public class AddEventActivity extends Activity {
 
+	/*
+	// Used for storing extras in intent
+	public static final String EXTRA_EVENT_TITLE = "event_title";
+    public static final String EXTRA_EVENT_START_DATE = "event_start_date";
+    public static final String EXTRA_EVENT_END_DATE = "event_end_date";
+    public static final String EXTRA_EVENT_START_TIME = "event_start_time";
+    public static final String EXTRA_EVENT_END_TIME= "event_end_time";
+    public static final String EXTRA_EVENT_LOCATION = "event_location";
+    public static final String EXTRA_EVENT_DESCRIPTION = "event_description";
+    public static final String EXTRA_EVENT_ADMISSION = "event_admission_fee";
+    
+    // Values for event fields
+    String mTitle, mStartDate, mStartTime, mEndDate, mEndTime, mLocation, mDescription;
+    boolean mAdmissionFee;
+    */
+	
 	Calendar myCalendar = Calendar.getInstance();
-	TextView startDateTextView, startTimeTextView, endDateTextView,
-			endTimeTextView;
 
+	// Variables for widgets in layout
+	TextView orgNameTextView, titleTextView, startDateTextView, startTimeTextView, endDateTextView,
+			endTimeTextView, locationTextView, descriptionTextView;
+	RadioGroup admissionRadioGroup;
+	Button saveButton;
+
+	// TODO - once AdminActivity created, change parent Activity in AndroidManifest.xml 
+	// TODO - once done with this activity, create EditEventActivity 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		// requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_add_event);
-
+	
 		// TODO - Update action bar to include X button instead of back
 		// TODO - Update action bar to include save button and remove from
 		// layout
 		ActionBar actionBar = getActionBar();
 		actionBar.setTitle("New Event");
 
-		// Attaching Date and Time TextViews
+		// cache all widgets
+		orgNameTextView = (TextView) findViewById(R.id.studentOrgName);
+		titleTextView = (TextView) findViewById(R.id.eventTitle);
 		startDateTextView = (TextView) findViewById(R.id.eventStartDate);
 		endDateTextView = (TextView) findViewById(R.id.eventEndDate);
 		startTimeTextView = (TextView) findViewById(R.id.eventStartTime);
 		endTimeTextView = (TextView) findViewById(R.id.eventEndTime);
+		locationTextView = (TextView) findViewById(R.id.eventLocation);
+		descriptionTextView = (TextView) findViewById(R.id.eventDescription);
+		admissionRadioGroup = (RadioGroup) findViewById(R.id.eventAdmissionRadioGroup);
+		saveButton = (Button) findViewById(R.id.save_event_button);
 
+		
+		// TODO - Restore settings (if editing event)
+		
+		
 		// OnClickListeners for Date and Time pickers
-		// TODO - setOnClickListener for both Time TextViews
 		startDateTextView.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -117,7 +153,7 @@ public class AddEventActivity extends Activity {
 	};
 
 	/**
-	 * Updates the TextView for the associated Date
+	 * Updates the associated Date TextView
 	 * 
 	 * @param tv
 	 *            The TextView (either starting date or ending date) to be
@@ -130,7 +166,7 @@ public class AddEventActivity extends Activity {
 	}
 
 	/**
-	 * Updates the TextView for the associated Time
+	 * Updates the associated Time TextView
 	 * 
 	 * @param tv
 	 *            The TextView (either starting time or ending time) to be
@@ -141,5 +177,19 @@ public class AddEventActivity extends Activity {
 		SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
 		tv.setText(sdf.format(myCalendar.getTime()));
 	}
+	
+	/*
+	protected void onSaveInstanceStat(Bundle b){
+        b.putString(EXTRA_EVENT_TITLE, mTitle);
+        b.putString(EXTRA_EVENT_START_DATE, mStartDate);
+        b.putString(EXTRA_EVENT_START_TIME, mStartTime);
+        b.putString(EXTRA_EVENT_END_DATE, mEndDate);
+        b.putString(EXTRA_EVENT_END_TIME, mEndTime);
+        b.putString(EXTRA_EVENT_LOCATION, mLocation);
+        b.putString(EXTRA_EVENT_DESCRIPTION, mDescription);
+        b.putBoolean(EXTRA_EVENT_ADMISSION, mAdmissionFee);
+        
+    }
+    */
 
 }
