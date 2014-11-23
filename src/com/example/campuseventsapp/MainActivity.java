1bc3b9243a0a70990f51f9e9b0f003f2be089db1
OJ7 <omairj7@gmail.com>
2014-11-23 16:12:42 -0500

    Added example implementation of map markers

--- src/com/example/campuseventsapp/MainActivity.java
+++ src/com/example/campuseventsapp/MainActivity.java
@@ -5,11 +5,16 @@ import com.example.campuseventsapp.R;
 import com.google.android.gms.internal.mm;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
 import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
@@ -24,6 +29,7 @@ public class MainActivity extends Activity {
 
 	private GoogleMap mMap;
 	private FloatingActionButton fabButton;
+	private Building stampBuilding;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -33,30 +39,46 @@ public class MainActivity extends Activity {
 		mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
 				.getMap();
 
-		//GoogleMapOptions options = new GoogleMapOptions();
-		//options.camera(CameraPosition.fromLatLngZoom(new LatLng(38.986918,
-		//-76.942554), 16));
-
 		// Centering Map on UMD
 		// LatLngBounds UMD = new LatLngBounds(new LatLng(38.981257, -76.95687),
 		// new LatLng(39.000962, -76.932355));
-
 		mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(38.989822,
-				-76.940637), 14));
+				-76.940637), 15));
 		mMap.getUiSettings().setZoomControlsEnabled(false);
 
+		// TODO - populate all buildings with markers (and change
+		// colors/visibility) once database of buildings is made
+
+		// Example Building and marker
+		stampBuilding = new Building("SSU",
+				"Adele H. Stamp Student Union Building", 163, 38.9880489,
+				-76.9444026);
+		addMarkers(stampBuilding);
+
+		mMap.setOnInfoWindowClickListener(new OnInfoWindowClickListener() {
+
+			@Override
+			public void onInfoWindowClick(Marker marker) {
+				// TODO open up list view with events from building specified in
+				// marker
+				String buildingName = marker.getTitle();
+
+			}
+		});
+
+		// Floating Action Button
 		fabButton = new FloatingActionButton.Builder(this)
 				.withDrawable(
 						getResources().getDrawable(R.drawable.ic_action_star))
 				.withButtonColor(Color.RED)
 				.withGravity(Gravity.BOTTOM | Gravity.RIGHT)
 				.withMargins(0, 0, 16, 16).create();
-
 		fabButton.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				// TODO - add more buttons when clicked
+				// TODO - implement material design animations
 				Intent intent = new Intent(MainActivity.this,
 						AddEventActivity.class);
 				startActivity(intent);
@@ -65,4 +87,16 @@ public class MainActivity extends Activity {
 
 	}
 
+	// Add a marker for specified building
+	private void addMarkers(Building building) {
+		// for (BuildingLocationRec rec : result) {}
+
+		mMap.addMarker(new MarkerOptions()
+				.position(new LatLng(building.getLat(), building.getLng()))
+				.title(building.getName())
+				.snippet("Events: " + String.valueOf(building.getNumEvents()))
+				.icon(BitmapDescriptorFactory
+						.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
+
+	}
 }
