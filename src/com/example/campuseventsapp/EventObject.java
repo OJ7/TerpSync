6067ca37b0e5e9ee062f10311418ffcdc7305aab
OJ7 <omairj7@gmail.com>
2014-11-27 00:23:40 -0500

    Cleaned up ParseObject classes

--- src/com/example/campuseventsapp/EventObject.java
+++ src/com/example/campuseventsapp/EventObject.java
@@ -10,77 +10,77 @@ public class EventObject extends ParseObject {
 
 	}
 
-	public void setOrgName(String OrgName) {
-		put("OrganizationName", OrgName);
-	}
-
-	public void setEventName(String EventName) {
-		put("EventName", EventName);
-	}
-
-	public void setBuildingName(String BuildName) {
-		put("BuildingName", BuildName);
-	}
-
-	public void setDescription(String desc) {
-		put("EventDescription", desc);
-	}
-
-	public void setStartDate(String date) {
-		put("StartDate", date);
-	}
-
-	public void setEndDate(String date) {
-		put("EndDate", date);
-	}
-
-	public void setEndTime(String time) {
-		put("EndTime", time);
-	}
-
-	public void setStartTime(String time) {
-		put("StartTime", time);
-	}
-
-	// this will be "FREE" if free, or a string representation of cost if paid
-	public void setAdmission(String cost) {
-		put("Admission", cost);
-	}
-
-	public String getAdmission() {
-		return getString("Admission");
-	}
-
 	public String getOrgName() {
-		return getString("OrganizatioName");
+		return getString("OrganizationName");
+	}
+
+	public void setOrgName(String orgName) {
+		put("OrganizationName", orgName);
 	}
 
 	public String getEventName() {
 		return getString("EventName");
 	}
 
-	public String getBuildingName() {
-		return getString("BuildingName");
-	}
-
-	public String getDescription() {
-		return getString("EventDescription");
+	public void setEventName(String eventName) {
+		put("EventName", eventName);
 	}
 
 	public String getStartDate() {
 		return getString("StartDate");
 	}
 
-	public String getEndDate() {
-		return getString("EndDate");
+	public void setStartDate(String date) {
+		put("StartDate", date);
 	}
 
 	public String getStartTime() {
 		return getString("StartTime");
 	}
 
+	public void setStartTime(String time) {
+		put("StartTime", time);
+	}
+
+	public String getEndDate() {
+		return getString("EndDate");
+	}
+
+	public void setEndDate(String date) {
+		put("EndDate", date);
+	}
+
 	public String getEndTime() {
 		return getString("EndTime");
+	}
+
+	public void setEndTime(String time) {
+		put("EndTime", time);
+	}
+
+	public String getBuildingName() {
+		return getString("BuildingName");
+	}
+
+	public void setBuildingName(String buildingName) {
+		put("BuildingName", buildingName);
+	}
+
+	public String getDescription() {
+		return getString("EventDescription");
+	}
+
+	public void setDescription(String desc) {
+		put("EventDescription", desc);
+	}
+
+	public String getAdmission() {
+		return getString("Admission");
+	}
+
+	// this will be "FREE" if free, or a string representation of cost if paid
+	public void setAdmission(String cost) {
+		put("Admission", cost);
 	}
 
 }
--- src/com/example/campuseventsapp/UMDBuildings.java
+++ src/com/example/campuseventsapp/UMDBuildings.java
@@ -6,10 +6,20 @@ import com.parse.ParseObject;
 @ParseClassName("UMDBuildings")
 public class UMDBuildings extends ParseObject {
 
-	private int numEvents;
-
 	public UMDBuildings() {
 
+	}
+
+	public String getName() {
+		return getString("name");
+	}
+
+	public String getBuildAbrev() {
+		return getString("code");
+	}
+
+	public String getBuildNum() {
+		return getString("Number");
 	}
 
 	public String getLat() {
@@ -17,34 +27,6 @@ public class UMDBuildings extends ParseObject {
 	}
 
 	public String getLng() {
-		return getString("Latitude");
+		return getString("longitude");
 	}
-
-	public String getBuildNum() {
-		return getString("Number");
-	}
-
-	public String getBuildAbrev() {
-		return getString("code");
-	}
-
-	public String getName() {
-		return getString("name");
-	}
-
-	/**
-	 * @return the number of events happening in that building during the
-	 *         current day
-	 */
-	public int getNumEvents() {
-		return numEvents;
-	}
-
-	/**
-	 * Returns string in form of: "(BuildingCode) BuildingName"
-	 */
-	public String toString() {
-		return "(" + getBuildAbrev() + ") " + getName();
-	}
-
 }
