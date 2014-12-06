2962866ee7f596e3044d08c450f8d87d41be6216
usik <usik.o2@gmail.com>
2014-12-06 16:43:57 -0500

    Fixed FAB bugs. Made the shadow darker

--- src/com/example/campuseventsapp/FloatingActionButton.java
+++ src/com/example/campuseventsapp/FloatingActionButton.java
@@ -55,7 +55,7 @@ public class FloatingActionButton extends View {
 		mButtonPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 		mButtonPaint.setColor(FloatingActionButtonColor);
 		mButtonPaint.setStyle(Paint.Style.FILL);
-		mButtonPaint.setShadowLayer(10.0f, 0.0f, 3.5f, Color.argb(100, 0, 0, 0));
+		mButtonPaint.setShadowLayer(10.0f, 0.0f, 3.5f, Color.argb(200, 0, 0, 0));
 		mDrawablePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
 		invalidate();
