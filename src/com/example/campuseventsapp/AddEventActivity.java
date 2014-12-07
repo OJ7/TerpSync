1b0b0e567fd16577f9f78ff56896bca6ba415b67
OJ7 <omairj7@gmail.com>
2014-12-06 19:52:32 -0500

    Fixed date and time strings not being correctly set in eventObject

--- src/com/example/campuseventsapp/AddEventActivity.java
+++ src/com/example/campuseventsapp/AddEventActivity.java
@@ -141,7 +141,7 @@ public class AddEventActivity extends Activity {
 				new DatePickerDialog(AddEventActivity.this, startDatePicker, myCalendar
 						.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar
 						.get(Calendar.DAY_OF_MONTH)).show();
-				
+
 			}
 		});
 		endDateTextView.setOnClickListener(new View.OnClickListener() {
@@ -207,7 +207,7 @@ public class AddEventActivity extends Activity {
 
 				// All valid input from User, create parse object and add to DB
 				if (formFilled) {
-				
+
 					eventObject.setOrgName(orgNameTextView.getText().toString());
 					eventObject.setEventName(eventNameEditText.getText().toString());
 					eventObject.setBuildingName(eventLocationTextView.getText().toString());
@@ -256,10 +256,12 @@ public class AddEventActivity extends Activity {
 			myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
 			updateDateLabel(startDateTextView);
 			Log.i("Campus-App", Integer.toString(monthOfYear));
-			eventObject.setStartDate(Integer.toString(monthOfYear) + "/"
+			// need to add 1 because the monthOfYear starts at 0
+			eventObject.setStartDate(Integer.toString(monthOfYear + 1) + "/"
 					+ Integer.toString(dayOfMonth) + "/" + Integer.toString(year));
 		}
 	};
+
 	DatePickerDialog.OnDateSetListener endDatePicker = new DatePickerDialog.OnDateSetListener() {
 		@Override
 		public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
@@ -267,11 +269,12 @@ public class AddEventActivity extends Activity {
 			myCalendar.set(Calendar.MONTH, monthOfYear);
 			myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
 			updateDateLabel(endDateTextView);
-			eventObject.setEndDate(Integer.toString(monthOfYear) + "/"
+			// need to add 1 because the monthOfYear starts at 0
+			eventObject.setEndDate(Integer.toString(monthOfYear + 1) + "/"
 					+ Integer.toString(dayOfMonth) + "/" + Integer.toString(year));
 		}
 	};
-	
+
 	TimePickerDialog.OnTimeSetListener startTimePicker = new TimePickerDialog.OnTimeSetListener() {
 		@Override
 		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
@@ -279,14 +282,14 @@ public class AddEventActivity extends Activity {
 			myCalendar.set(Calendar.MINUTE, minute);
 			updateTimeLabel(startTimeTextView);
 			if (myCalendar.get(Calendar.AM_PM) == Calendar.AM) {
-				eventObject.setStartTime(Integer.toString(hourOfDay) + ":"
-						+ Integer.toString(minute) + " AM");
+				eventObject.setEndTime(String.format("%02d:%02d", hourOfDay, minute) + " AM");
 			} else {
-				eventObject.setStartTime(Integer.toString(hourOfDay) + ":"
-						+ Integer.toString(minute) + " PM");
+				eventObject.setEndTime(String.format("%02d:%02d", hourOfDay, minute) + " PM");
 			}
+
 		}
 	};
+
 	TimePickerDialog.OnTimeSetListener endTimePicker = new TimePickerDialog.OnTimeSetListener() {
 		@Override
 		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
@@ -294,11 +297,9 @@ public class AddEventActivity extends Activity {
 			myCalendar.set(Calendar.MINUTE, minute);
 			updateTimeLabel(endTimeTextView);
 			if (myCalendar.get(Calendar.AM_PM) == Calendar.AM) {
-				eventObject.setEndTime(Integer.toString(hourOfDay) + ":" + Integer.toString(minute)
-						+ " AM");
+				eventObject.setEndTime(String.format("%02d:%02d", hourOfDay, minute) + " AM");
 			} else {
-				eventObject.setEndTime(Integer.toString(hourOfDay) + ":" + Integer.toString(minute)
-						+ " PM");
+				eventObject.setEndTime(String.format("%02d:%02d", hourOfDay, minute) + " PM");
 			}
 		}
 	};
