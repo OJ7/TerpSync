1df4758bf1f1ece170d17db56daaab508f36b743
OJ7 <omairj7@gmail.com>
2014-12-07 22:05:13 -0500

    Added onPause and onResume

--- src/com/example/campuseventsapp/MainActivity.java
+++ src/com/example/campuseventsapp/MainActivity.java
@@ -130,6 +130,19 @@ public class MainActivity extends Activity {
 		currentOrganization = settings.getString("currentOrganization", currentOrganization);
 	}
 
+	private void savePreferences() {
+		// We need an Editor object to make preference changes.
+		// All objects are from android.context.Context
+		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+		SharedPreferences.Editor editor = settings.edit();
+		editor.putInt("adminToggle", adminToggle);
+		editor.putString("currentUser", currentUser);
+		editor.putString("currentOrganization", currentOrganization);
+
+		// Commit the edits!
+		editor.commit();
+	}
+
 	/*
 	 * Creates ALL FAB Views
 	 */
@@ -888,19 +901,21 @@ public class MainActivity extends Activity {
 	}
 
 	@Override
+	protected void onResume() {
+		super.onResume();
+		restorePreferences();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		savePreferences();
+	}
+
+	@Override
 	protected void onStop() {
 		super.onStop();
-
-		// We need an Editor object to make preference changes.
-		// All objects are from android.context.Context
-		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-		SharedPreferences.Editor editor = settings.edit();
-		editor.putInt("adminToggle", adminToggle);
-		editor.putString("currentUser", currentUser);
-		editor.putString("currentOrganization", currentOrganization);
-
-		// Commit the edits!
-		editor.commit();
+		savePreferences();
 	}
 
 }
