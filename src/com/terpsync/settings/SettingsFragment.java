7eba7283899473c3ea7f0280bb20c1cba05b6e5c
OJ7 <omairj7@gmail.com>
2015-02-02 02:11:25 -0500

    Implemented changing account info in settings
    Finished with the settings, fully replacing the admin panel.
    Resolves issue #47

--- src/com/terpsync/settings/SettingsFragment.java
+++ src/com/terpsync/settings/SettingsFragment.java
@@ -1,26 +1,52 @@
 package com.terpsync.settings;
 
+import java.util.List;
+import java.util.Locale;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.terpsync.R;
 import com.terpsync.SignInActivity;
+import com.terpsync.parse.AdminAccounts;
+import com.terpsync.parse.ParseConstants;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceFragment;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class SettingsFragment extends PreferenceFragment {
 
 	private static final String TAG = "SettingsFragment";
 	public static final String PREFS_NAME = "MyPrefsFile";
+	private Object lock = new Object();
 
 	// Global variable strings used for preferences
 	private final String signedInPref = "isSignedIn", currentUserPref = "currentUser",
 			currentOrgPref = "currentOrganization";
 
 	Preference signInPreference;
+	Builder manageAccountDialog, changeSignInBuilder;
+	String[] manageAccountOptions = { "Change username/password", "Sign out" };
+	AlertDialog myDialog;
+	EditText mUserView, mPasswordView, mConfirmView;
+	Button mChangeButton;
+	View changeSignInView;
+	AlertDialog changeSignInDialog;
+	boolean cancel;
+	View focusView;
 
 	// Global variables for Current User (if signed in)
 	boolean isSignedIn = false;
@@ -79,17 +105,15 @@ public class SettingsFragment extends PreferenceFragment {
 	 * Modify preference if user is signed in
 	 */
 	private void changeToSignedIn() {
-		signInPreference.setTitle(R.string.pref_title_admin_sign_out);
+		signInPreference.setTitle(R.string.pref_title_admin_manage);
 		signInPreference.setSummary(this.getActivity().getString(
 				R.string.pref_summary_admin_signed_in_as)
 				+ " " + currentOrganization);
-		// Sign out dialog when clicked
+		// Manage account dialog when clicked
 		signInPreference.setOnPreferenceClickListener(new OnPreferenceClickListener() {
 			@Override
 			public boolean onPreferenceClick(Preference preference) {
-				// TODO - add dialog
-				signOutAdmin();
-				signInPreference.setOnPreferenceClickListener(null);
+				showManageAccountDialog();
 				return true;
 			}
 		});
@@ -101,6 +125,7 @@ public class SettingsFragment extends PreferenceFragment {
 	private void changeToSignedOut() {
 		signInPreference.setTitle(R.string.pref_title_admin_sign_in);
 		signInPreference.setSummary(R.string.pref_summary_admin_sign_in);
+		signInPreference.setOnPreferenceClickListener(null);
 		Intent intent = new Intent(this.getActivity(), SignInActivity.class);
 		signInPreference.setIntent(intent);
 	}
@@ -117,6 +142,184 @@ public class SettingsFragment extends PreferenceFragment {
 		isSignedIn = false;
 		changeToSignedOut();
 		savePreferences();
+	}
+
+	/**
+	 * Creates a dialog menu with options to change username/password or sign out.
+	 */
+	private void showManageAccountDialog() {
+		Log.i(TAG, "Creating Manage Account Dialog");
+		manageAccountDialog = new AlertDialog.Builder(this.getActivity());
+		manageAccountDialog.setTitle("Manage Account")
+				.setItems(manageAccountOptions, new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int item) {
+						switch (item) {
+							case 0: // Change UN/PW
+								showChangeSignInDialog();
+								break;
+							case 1: // Sign out
+								signOutAdmin();
+								break;
+							default:
+								break;
+						}
+					}
+				}).create().show();
+	}
+
+	/**
+	 * Creates a dialog box to change username and/or password.
+	 */
+	private void showChangeSignInDialog() {
+		Log.i(TAG, "Creating Change Account Info Dialog");
+
+		// Inflating View
+		changeSignInView = this.getActivity().getLayoutInflater()
+				.inflate(R.layout.dialog_changesignin, null);
+		// Caching fields
+		mUserView = (EditText) changeSignInView.findViewById(R.id.newUsername);
+		mPasswordView = (EditText) changeSignInView.findViewById(R.id.newPassword);
+		mConfirmView = (EditText) changeSignInView.findViewById(R.id.newPasswordConfirm);
+		mChangeButton = (Button) changeSignInView.findViewById(R.id.changeButton);
+
+		// Creating dialog box
+		changeSignInBuilder = new AlertDialog.Builder(this.getActivity());
+		changeSignInDialog = changeSignInBuilder.setView(changeSignInView)
+				.setTitle("Update Account Info").create();
+		changeSignInDialog.show();
+
+		mChangeButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				attemptChangeAccountInfo();
+			}
+		});
+
+	}
+
+	/**
+	 * Attempts to change username and/or password (depending on what is specified). If username
+	 * already exists and/or passwords do not match, the changes will not be made.
+	 * 
+	 * Requirements: Usernames and passwords must be at least three characters in length.
+	 * 
+	 * @return true if changed successfully, false otherwise.
+	 */
+	private void attemptChangeAccountInfo() {
+		Log.i(TAG, "Attempting to change sign in credentials");
+
+		// Reset errors.
+		mUserView.setError(null);
+		mPasswordView.setError(null);
+		mConfirmView.setError(null);
+
+		// Store values at the time of the login attempt.
+		final String username = mUserView.getEditableText().toString().toLowerCase(Locale.US)
+				.trim();
+		final String password = mPasswordView.getEditableText().toString();
+		final String confirm = mConfirmView.getEditableText().toString();
+
+		cancel = false;
+		focusView = null;
+
+		// Check for a valid password, if the user entered one.
+		if (!isPasswordValid(password)) {
+			mPasswordView.setError(getString(R.string.error_invalid_password));
+			focusView = mPasswordView;
+			cancel = true;
+		}
+		// Checks if passwords are equal
+		else if (!password.equals(confirm)) {
+			// TODO - change string for below
+			mConfirmView.setError(getString(R.string.error_not_matching_password));
+			focusView = mConfirmView;
+			cancel = true;
+		}
+
+		// Check for a valid username.
+		if (!isUserValid(username)) {
+			mUserView.setError(getString(R.string.error_invalid_username));
+			focusView = mUserView;
+			cancel = true;
+		} else {
+			// confirm username does not already exist
+			ParseQuery<AdminAccounts> query = ParseQuery.getQuery(AdminAccounts.class);
+			if (!username.equals(currentUser)) { // allow keeping same username
+				query.whereContains(ParseConstants.admin_username, username);
+				query.findInBackground(new FindCallback<AdminAccounts>() {
+					@Override
+					public void done(List<AdminAccounts> arg0, ParseException arg1) {
+						if (arg0 == null) {
+							Log.i(TAG, "Username already exists");
+							synchronized (lock) {
+								mUserView.setError(getString(R.string.error_existing_username));
+								focusView = mUserView;
+								cancel = true;
+							}
+						}
+					}
+				});
+			}
+		}
+
+		if (cancel) {
+			// There was an error; don't attempt changes and focus the first
+			// form field with an error.
+			focusView.requestFocus();
+		} else {
+			// Perform changes to account
+			ParseQuery<AdminAccounts> query = ParseQuery.getQuery(AdminAccounts.class);
+			query.whereContains(ParseConstants.admin_username, currentUser);
+			query.findInBackground(new FindCallback<AdminAccounts>() {
+				@Override
+				public void done(List<AdminAccounts> arg0, ParseException arg1) {
+					if (arg0 == null || arg0.size() == 0) {
+						Log.d(TAG, "The current signed in user is probably set to a different user");
+						Toast.makeText(
+								getActivity().getBaseContext(),
+								"Error changing username/password.\nTry again after signing out and in.",
+								Toast.LENGTH_SHORT).show();
+					} else if (arg1 == null) {
+						AdminAccounts x = arg0.get(0);
+						x.setUsername(username);
+						x.setPassword(password);
+						x.saveInBackground();
+						currentUser = username;
+						Log.i(TAG, "Change successful!");
+						Log.i(TAG, "New username: " + username);
+						Log.i(TAG, "New password:" + password);
+						Toast.makeText(
+								getActivity().getBaseContext(),
+								"Username/Password changed successfully: [" + username + "]" + ":["
+										+ password + "]", Toast.LENGTH_SHORT).show();
+					} else { // object retrieval failed throw exception -- fail fast
+						arg1.printStackTrace();
+					}
+					resetChangeSignInDialog();
+				}
+			});
+		}
+	}
+
+	private boolean isUserValid(String user) {
+		return user.length() > 3;
+	}
+
+	private boolean isPasswordValid(String password) {
+		return password.length() > 3;
+	}
+
+	/**
+	 * Resets the change sign in dialog to clear text and remove from view
+	 */
+	private void resetChangeSignInDialog() {
+		Log.i(TAG, "Resetting fields in change sign in credentials dialog");
+		mUserView.getText().clear();
+		mPasswordView.getText().clear();
+		mConfirmView.getText().clear();
+		((ViewGroup) changeSignInView.getParent()).removeView(changeSignInView);
+		changeSignInDialog.dismiss();
 	}
 
 	@Override
